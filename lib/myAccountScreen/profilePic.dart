1be218530cb114d0daaa5535a619cad278f7f1c2
Varad Shinde <99754991+Admsher@users.noreply.github.com>
2022-10-18 15:59:12 +0530

    Update profilePic.dart

--- lib/myAccountScreen/profilePic.dart
+++ lib/myAccountScreen/profilePic.dart
@@ -15,93 +15,110 @@ class profilePic extends StatelessWidget {
               color: Colors.white,
               margin: EdgeInsets.fromLTRB(0, 134, 0, 0),
               padding: EdgeInsets.fromLTRB(105, 0, 50, 140)),
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-            child: Card(
-                child: SizedBox(
-              width: 500,
-              height: 198,
-              child: Column(
-                children: [
-                  Container(
-                      padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                      child: Column(children: [
-                        Text(
-                          'Julia Parker',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          'julia@gmail.com',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              
-                                      Container(
-                                        margin:
-                                            const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                                        child: ElevatedButton(
-                                          onPressed: () {},
-                                          style: ElevatedButton.styleFrom(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 40, vertical: 30),
-                                            backgroundColor: Color.fromARGB(
-                                                255, 113, 110, 110),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(12)),
-                                          ),
-                                          child: const Text(
-                                            'Trips',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Card(
+                    child: SizedBox(
+                  width: 500,
+                  height: 198,
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                          child: Column(children: [
+                            Text(
+                              'Julia Parker',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'julia@gmail.com',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  
+                                          Container(
+                                            margin:
+                                                const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                            child: ElevatedButton(
+                                              onPressed: () {},
+                                              style: ElevatedButton.styleFrom(
+                                                padding: const EdgeInsets.symmetric(
+                                                    horizontal: 40, vertical: 20),
+                                                backgroundColor: Color.fromARGB(
+                                                    255, 113, 110, 110),
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(12)),
+                                              ),
+                                              child:  Column(
+                                                children: [
+                                                  Icon(Icons.hiking_rounded
+                                                      color:Colors.black),
+                                                  Text(
+                                                    'Trips',
+                                                    style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      ),
-                                      Container(
-                                        margin:
-                                            const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                                        child: ElevatedButton(
-                                          onPressed: () {},
-                                          style: ElevatedButton.styleFrom(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 40, vertical: 30),
-                                            backgroundColor: Color.fromARGB(
-                                                255, 113, 110, 110),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(12)),
-                                          ),
-                                          child: const Text(
-                                            'Share',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: "Lato",
-                                              color: Colors.black,
+                                          Container(
+                                            margin:
+                                                const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                            child: ElevatedButton(
+                                              onPressed: () {},
+                                              style: ElevatedButton.styleFrom(
+                                                padding: const EdgeInsets.symmetric(
+                                                    horizontal: 40, vertical: 20),
+                                                backgroundColor: Color.fromARGB(
+                                                    255, 113, 110, 110),
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(12)),
+                                              ),
+                                              child:Column(
+                                                children: [
+                                                  Icon(Icons.share_location
+                                                      color:Colors.black),
+                                                  Text(
+                                                    'Share',
+                                                    style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      fontFamily: "Lato",
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      ),
-                                    ],
-                                  ))
-                            ],
-                          ),
-                        ),
-                      ])),
-                
-              ),
-            ),
+                                        ],
+                                      ))
+                                ],
+                              ),
+                            ),
+                          ])),
+                    
+                  ),
+                ),
+            ],
+          ),
           
           Container(
             margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
@@ -116,4 +133,25 @@ class profilePic extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
